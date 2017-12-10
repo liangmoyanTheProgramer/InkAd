b4ca210d14bc4e13601db63816a8b1f83fbf22eb
liangmoyanTheProgramer <1792070600@qq.com>
2017-12-10 16:05:13 +0800

    Initial commit

---
+++ app/src/main/java/io/github/development/liang/moyan/inkad/AdActivity.java
@@ -0,0 +1,13 @@
+package io.github.development.liang.moyan.inkad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad);
+    }
+}
